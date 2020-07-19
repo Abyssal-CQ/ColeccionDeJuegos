55b0151df69a86b202b4266e8d8612d3492f3988
unknown <carlos.quispe.q@tecsup.edu.pe>
2020-07-19 10:46:28 -0500

    Initial Commit

---
+++ ColeccionDeJuegos/ViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  ColeccionDeJuegos
+//
+//  Created by Oswaldo Naupa Laura on 6/16/20.
+//  Copyright © 2020 deah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return juegos.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        let juego = juegos[indexPath.row]
+        cell.textLabel?.text = juego.titulo
+        cell.imageView?.image = UIImage(data: (juego.imagen!))
+        return cell
+    }
+    
+
+    @IBAction func agregarPhoto(_ sender: Any) {
+        performSegue(withIdentifier: "agregarSegue", sender: nil)
+    }
+    
+    @IBOutlet weak var tableView: UITableView!
+    var juegos : [Juego] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        do {
+            try juegos = context.fetch(Juego.fetchRequest())
+            tableView.reloadData()
+        } catch  {
+        }
+    }
+
+}
+
